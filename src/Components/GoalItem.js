2ed0633f5b921c973b941597215aeb633acfb0fe
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-10-05 14:53:55 +0300

    adding the handlechange function for Patch method

--- src/Components/GoalItem.js
+++ src/Components/GoalItem.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React,  {useState} from 'react';
 
 
 function GoalItem() {
      const {id, name, progress} = goal;
     const [progressBar, setProgressBar] = useState(progress);
-    
+     
+    const handleChange = (e) => {
+        setProgressBar(parseInt(e.target.value))
+        fetch(`http://localhost:8000/goals/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({progress: progressBar})
+        })
+            .then(res => res.json())
+            .then((updatedGoal) => {
+                onUpdateGoal(updatedGoal)
+            })
+    }
+
+    const handleDelete = () => {
+        let type = "goals";
+        onDeleteClick(id, type)
+    }
     
     return (
     
