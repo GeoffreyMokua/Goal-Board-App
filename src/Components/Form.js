1adef4382e6f233ce0a438c683c98c12cc817ba8
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-10-05 12:28:12 +0300

    importing the useState hook and setting the state variables

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Form() {
+    const [formType, setFormType] = useState(true);
+    const [goalData, setGoalData] = useState({
+        name: "",
+        progess: ""
+    })
     
 
 
     return (
         <section>
-        
-        
-        
         
         
         </section>
