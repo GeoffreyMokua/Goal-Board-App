a0a483862e1e375fe816c38dc6576d287302b06b
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-10-05 15:35:19 +0300

    importing function components and adding state variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import { Route, Switch } from "react-router-dom";
 
-import './App.css';
+import NavBar from "./components/NavBar";
+import Goals from "./components/Goals";
+import Accomplishments from "./components/Accomplishments";
+import Home from "./components/Home";
+import Form from "./components/Form";
+
 
 function App() {
+  const [goals, setGoals] = useState([]);
+  const [accomplishments, setAccomplishments] = useState([]);
+  
   return (
     <div>
-      <h1>hello world</h1>
+      <h1>hello world problem</h1>
     </div>
   );
 }
