dcb26131e6889bafb1d7535208ed2f9b3befeb89
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-10-05 12:53:01 +0300

    returning all the functions to get data

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -55,9 +55,49 @@ function Form() {
 
 
     return (
-        <section>
-        
-        
+       <section id="form-section">
+            <h2>Add a new goal or an accomplishment:</h2>
+            <form onSubmit={handleSubmit}>
+                <select 
+                    name="form-type"
+                    onChange={handleFormTypeChange}
+                    >
+                    <option value="goal">Goal</option>
+                    <option value="accomplishment">Accomplishment</option>
+                </select>
+                <input 
+                    onChange={handleChange}
+                    className="description"
+                    type="text"
+                    placeholder="Description"
+                    name="name"
+                    value={formType ? goalData.name : accomplishmentData.name}
+                    />
+                
+                { 
+                // Ternary operator to determine which kind of form we are completing, based off of the dropdown selection.
+                formType ?
+                    <label>% Complete
+                    <input 
+                        onChange={handleChange}
+                        type="number"
+                        placeholder="0"
+                        name="progress"
+                        value={goalData.progress}
+                        />
+                    </label> : 
+                    <label>Completion Date:
+                    <input 
+                        onChange={handleChange}
+                        type="text"
+                        placeholder="Ex. May 2020"
+                        name="completed"
+                        value={accomplishmentData.completed}
+                        />
+                    </label>
+                }
+                <button type="submit">Add</button>
+            </form>
         </section>
 
 
